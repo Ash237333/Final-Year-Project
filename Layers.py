139fbe6e8e92496c46a8a76bab8f0aca6018c11d
Ash23733 <stephen.bromley04@gmail.com>
2024-10-22 21:21:21 +0100

    Finished encoder stack

---
+++ Layers.py
@@ -0,0 +1,55 @@
+import torch
+from torch import nn
+
+EMBEDDING_DIMENSION = 100
+
+def positional_encoder(input):
+    """
+    Defines an encoder for adding positional information to vector embeddings
+
+    :param input: Tensor after batch embeddings (seq_length, batch_size, embedding_dim)
+    :return: Positional encoding added to embedding dim (seq_length, batch_size, embedding_dim)
+    """
+
+    # Initialises the result tensor as an empty tensor of shape (seq_length, embedding_dim)
+    seq_length, _, embedding_dim = input.shape
+    result = torch.empty(seq_length, embedding_dim)
+
+    # Calculates the denominator taking into account the difference for even and odd embedding positions
+    positions = torch.arange(start=0, end=seq_length, step=1, dtype=torch.float).unsqueeze(1)
+    embedding_positions = torch.arange(start=0, end=embedding_dim, step=1, dtype=torch.float)
+    calculate_i = (embedding_positions // 2) * 2
+    divisor = 10000 ** (calculate_i / embedding_dim)
+
+    # Calculates the full term within the sin/cos function
+    positions = positions / divisor
+
+    # Applies sin/cos based on if the number is odd or even
+    result[:, ::2] = torch.sin(positions[:, ::2])
+    result[:, 1::2] = torch.cos(positions[:, 1::2])
+
+    # Adds the positional encoding to the embedded vectors
+    result = input + result.unsqueeze(1)
+
+    return result
+
+class Encoder_Layer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mha = nn.MultiheadAttention(embed_dim=EMBEDDING_DIMENSION, num_heads=8)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(EMBEDDING_DIMENSION, EMBEDDING_DIMENSION),
+            nn.ReLU(),
+            nn.Linear(EMBEDDING_DIMENSION, EMBEDDING_DIMENSION)
+        )
+        self.layer_norm = nn.LayerNorm(EMBEDDING_DIMENSION)
+
+    def forward(self, input):
+        #Multi-headed attention
+        mha, _ = self.mha(input, input, input)
+        mha = self.layer_norm(input + mha)
+
+        #Feed Forward network
+        feed_forward = self.feed_forward(mha)
+        feed_forward = self.layer_norm(mha + feed_forward)
+        return feed_forward
--- Model.py
+++ Model.py
@@ -1,55 +1,34 @@
-import torch
 from torch import nn
 from Dataloader import VOCAB_SIZE
+import Layers
+from Layers import Encoder_Layer, EMBEDDING_DIMENSION
 
+NUM_ENCODER_LAYERS = 6
 
 class Seq2seq(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self):
         """
         Defines all layers to be used in the neural network
         """
         super().__init__()
-        self.embed = nn.Embedding(VOCAB_SIZE, embedding_dim=100)
+        self.embed = nn.Embedding(VOCAB_SIZE, embedding_dim=EMBEDDING_DIMENSION)
+        self.encoder_layer_stack = nn.ModuleList([Encoder_Layer() for _ in range(NUM_ENCODER_LAYERS)])
+
 
     def forward(self, x):
         """
         Defines the order which tensors are passed through the layers in the neural network
 
-        :param x: The inputted tensor
+        :param x: Batch of sequences of int labels, (Seq_length, Batch_size)
         :return: The outputted tensor
         """
 
+        #Turns sequence of int labels into vector encodings + positional information
+
         embedded_vectors = self.embed(x)
-        pos_added_vectors = self.positional_encoder(embedded_vectors)
+        pos_added_vectors = Layers.positional_encoder(embedded_vectors)
+
+        for layer in self.encoder_layer_stack:
+            pos_added_vectors = layer(pos_added_vectors)
+
         return pos_added_vectors
-
-    def positional_encoder(self, input):
-        """
-        Defines the positional encoder.
-
-        :param input: The inputted tensor after vector embedding
-        :return: A tensor with the same shape as the input but with the positional
-        encoding added to the third dimension.
-        """
-
-        #Initialises the result tensor as an empty tensor of shape (seq_length, embedding_dim)
-        seq_length, batch_size, embedding_dim = input.shape
-        result = torch.empty(seq_length, embedding_dim)
-
-        #Calculates the denominator taking into account the difference for even and odd embedding positions
-        positions = torch.arange(start=0, end=seq_length, step=1, dtype=torch.float).unsqueeze(1)
-        embedding_positions = torch.arange(start=0, end=embedding_dim, step=1, dtype=torch.float)
-        calculate_i = (embedding_positions // 2) * 2
-        divisor = 10000 ** (calculate_i / embedding_dim)
-
-        #Calculates the full term within the sin/cos function
-        positions = positions / divisor
-
-        #Applies sin/cos based on the numbers parity
-        result[:, ::2] = torch.sin(positions[:, ::2])
-        result[:, 1::2] = torch.cos(positions[:, 1::2])
-
-        #Adds the positional encoding to the embedded vectors
-        result = input + result.unsqueeze(1)
-
-        return result

3ca1b4dc563db6e1002aa462bbf0269fbf3d9a94
Ash23733 <stephen.bromley04@gmail.com>
2024-11-20 16:23:00 +0000

    Added tokenizer preprocessing for the dataset when loaded before training

--- Dataloader.py
+++ Dataloader.py
@@ -4,16 +4,17 @@ from tqdm import tqdm
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
+from torch.utils.data import DataLoader
 
 
 GERMAN_FILEPATH = "German_text.txt"
 ENGLISH_FILEPATH = "English_text.txt"
 VOCAB_SIZE = 30000
 
-if not (os.path.exists(GERMAN_FILEPATH) and os.path.exists(ENGLISH_FILEPATH)):
+def save_dataset_as_text():
     ds = load_dataset("wmt/wmt14", "de-en", split="test")
-
-    with open(GERMAN_FILEPATH, 'w', encoding='utf-8') as german_file, open(ENGLISH_FILEPATH, 'w', encoding='utf-8') as english_file:
+    with open(GERMAN_FILEPATH, 'w', encoding='utf-8') as german_file, open(ENGLISH_FILEPATH, 'w',
+                                                                           encoding='utf-8') as english_file:
         for x in tqdm(ds):
             gtext = x["translation"]["de"]
             etext = x["translation"]["en"]
@@ -24,7 +25,36 @@ if not (os.path.exists(GERMAN_FILEPATH) and os.path.exists(ENGLISH_FILEPATH)):
             english_file.write(etext + " ")
             german_file.write(gtext + " ")
 
-BPE_tokenizer = Tokenizer(BPE())
-BPE_trainer = BpeTrainer(vocab_size=VOCAB_SIZE, show_progress=True)
-BPE_tokenizer.train([ENGLISH_FILEPATH],BPE_trainer)
-BPE_tokenizer.save("BPE_Tokenizer.json")
+def train_tokenizer():
+    if not (os.path.exists(GERMAN_FILEPATH) and os.path.exists(ENGLISH_FILEPATH)):
+        save_dataset_as_text()
+
+    BPE_tokenizer = Tokenizer(BPE())
+    BPE_trainer = BpeTrainer(vocab_size=VOCAB_SIZE, show_progress=True)
+    BPE_tokenizer.train([ENGLISH_FILEPATH], BPE_trainer)
+    BPE_tokenizer.save("BPE_Tokenizer.json")
+
+def load_dataset_and_preprocess():
+    train_dataset = load_dataset("wmt/wmt14", "de-en", split="train")
+    test_dataset = load_dataset("wmt/wmt14", "de-en", split="test")
+
+    train_dataset = tokenize_dataset(train_dataset)
+    test_dataset = tokenize_dataset(test_dataset)
+
+    return train_dataset, test_dataset
+
+def tokenize_dataset(dataset):
+    BPE_tokenizer = Tokenizer(BPE())
+    BPE_tokenizer.from_file("BPE_Tokenizer.json")
+
+    def tokenize_func(pair):
+        gtext_tokenized = BPE_tokenizer.encode(pair["translation"]["de"]).ids
+        etext_tokenized = BPE_tokenizer.encode(pair["translation"]["en"]).ids
+        return {
+            "input_ids": gtext_tokenized,
+            "attention_mask": [1] * len(gtext_tokenized),
+            "labels": etext_tokenized
+        }
+
+    tokenized_dataset = dataset.map(tokenize_func, remove_columns=["translation"])
+    return tokenized_dataset
---
+++ Training.py
@@ -0,0 +1,13 @@
+import Dataloader
+from Model import Transformer
+
+EPOCHS = 20
+train, test = Dataloader.load_dataset()
+
+def train_one_epoch():
+    return
+
+def train():
+    for epoch in range(EPOCHS):
+        train_one_epoch()
+    return

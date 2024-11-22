983732d4dce2c11db94f314a3f415ed4e1d05c97
Ash23733 <stephen.bromley04@gmail.com>
2024-11-22 17:19:20 +0000

    Added dynamic padding

--- Dataloader.py
+++ Dataloader.py
@@ -1,9 +1,13 @@
+from cProfile import label
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from transformers import PreTrainedTokenizerFast
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from transformers.models.gpt_neox.modeling_gpt_neox import attention_mask_func
 
 VOCAB_SIZE = 30000
 BATCH_SIZE = 100
@@ -53,3 +57,20 @@ def create_dataloader():
     train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test, batch_size=BATCH_SIZE, shuffle=False)
     return train_loader, test_loader
+
+def collate_fn(batch):
+    input_ids, attention_mask, labels = [], [], []
+    for i in batch:
+        input_ids.append(i["input_ids"])
+        attention_mask.append(i["attention_mask"])
+        labels.append(i["labels"])
+
+    padded_input_ids = pad_sequence(input_ids, batch_first=True)
+    padded_attention_mask = pad_sequence(attention_mask, batch_first=True)
+    padded_labels = pad_sequence(labels, batch_first=True)
+
+    return {
+        "input_ids": padded_input_ids,
+        "attention_mask": padded_attention_mask,
+        "labels": padded_labels,
+    }
